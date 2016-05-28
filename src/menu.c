7695217d32b1f057fb449bc37ed25d722e6516b5
Martin Homuth <martin.homuth@googlemail.com>
2016-05-28 16:32:14 +0200

    revisit the introductional tutorial

--- src/menu.c
+++ src/menu.c
@@ -3,46 +3,47 @@
 #include <unistd.h>   // So we got the profile for 10 seconds#include <stdio.h>
 #include <stdio.h>
 
-#define NIL (0)       // A name for the void pointer
-
 int main()
 {
 
-	Display *dpy = XOpenDisplay(NIL);
+	/* connect to the X server */
+	Display *dpy = XOpenDisplay(NULL);
 	assert(dpy);
 
+	/* black and white are the only asserted colors I can get on every machine */
 	int blackColor = BlackPixel(dpy, DefaultScreen(dpy));
 	int whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
 
 	/* Create the window */
 	Window w = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0,
-				     200, 100, 0, blackColor, blackColor);
-	// We want to get MapNotify events
+				     200, 100, 1, whiteColor, blackColor);
 
-	XSelectInput(dpy, w, StructureNotifyMask);
+	/* register for structure events */
+	XSelectInput(dpy, w, StructureNotifyMask | ConfigureNotify);
 
-	// Create a "Graphics Context"
+	/* "map" the window */
+	XMapWindow(dpy, w); /* this is an asynchronous call */
 
-	GC gc = XCreateGC(dpy, w, 0, NIL);
+	/* Create a "Graphics Context" */
+	/* This is used to store drawing settings such as line thickness */
+	GC gc = XCreateGC(dpy, w, 0, NULL);
 
-	// Tell the GC we draw using the white color
-
+	/* set the drawing color */
 	XSetForeground(dpy, gc, whiteColor);
 
-	// "Map" the window (that is, make it appear on the screen)
-
-	XMapWindow(dpy, w);
-
-	// Wait for the MapNotify event
-
+	/* The event loop waiting for events from the server */
 	for(;;) {
 		XEvent e;
 		XNextEvent(dpy, &e);
 		if (e.type == MapNotify)
 			printf("mapNotify event\n");
 
+		if (e.type == GraphicsExpose)
+			printf("GraphicsExpose event\n");
+
 		XDrawLine(dpy, w, gc, 10, 180, 180, 20);
 		XDrawLine(dpy, w, gc, 10, 20, 180, 180);
+		/* tell the server what to do */
 		XFlush(dpy);
 
 	}
