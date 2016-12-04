b1ad8f53ba5e9b8e5a80c5c4522fe2a8de9fc826
Martin Homuth <martin@martinhomuth.de>
2016-12-04 18:57:57 +0100

    add benchmark
    Signed-off-by: Martin Homuth <martin@martinhomuth.de>

--- src/pearls/bitmap_sort.c
+++ src/pearls/bitmap_sort.c
@@ -7,6 +7,17 @@
 
 #define LENGTH 10000000
 
+
+/**
+   $ wc integers.txt
+   10000000 10000000 78890375 integers.txt
+
+   $ time sort -n integers.txt >/dev/null
+   sort -n integers.txt > /dev/null  23.07s user 0.58s system 280% cpu 8.425 total
+
+   $ time bin/0202_bitmap_sort < integers.txt > /dev/null
+   bin/0202_bitmap_sort < integers.txt > /dev/null  2.41s user 0.04s system 99% cpu 2.442 total
+*/
 int main(void)
 {
 	long readlong = 0L;
