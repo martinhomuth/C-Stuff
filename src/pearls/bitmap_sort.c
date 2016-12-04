8546f9f0228a2f7782efb8c3c5935db953bc5ec9
Martin Homuth <martin@martinhomuth.de>
2016-12-04 18:40:13 +0100

    add bitmap sorting
    Signed-off-by: Martin Homuth <martin@martinhomuth.de>

---
+++ src/pearls/bitmap_sort.c
@@ -0,0 +1,26 @@
+#include "bitvectors.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define LENGTH 10000000
+
+int main(void)
+{
+	long readlong = 0L;
+	void *bitmap = bitvector_new(LENGTH);
+
+	while(scanf("%ld", &readlong) != EOF) {
+		printf("setting %ld\n", readlong);
+		bitvector_set(bitmap, readlong);
+	}
+
+	for (long i = 0; i < LENGTH; i++)
+		if (bitvector_test(bitmap, i))
+			printf("%ld\n", i);
+
+	bitvector_delete(bitmap);
+	return 0;
+}
