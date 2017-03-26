be20b9004c1af1e6793ad1fd8d2795274618d34b
Martin Homuth <martin@martinhomuth.de>
2017-03-26 21:20:44 +0200

    Set: add, drop, find, contains and differ
    Signed-off-by: Martin Homuth <martin@martinhomuth.de>

--- src/oopc/Set.c
+++ src/oopc/Set.c
@@ -5,8 +5,11 @@
 #include <assert.h>
 
 #if ! defined MANY || MANY < 1
-#define MANY 10
+#define MANY 10 /* impossible index value */
 #endif
+
+const void *Set;
+const void *Object;
 
 /**
  * Location to store objects
@@ -46,3 +49,73 @@ void delete(void *_item)
 	}
 }
 
+/**
+ * @brief Adds an element to a set.
+ *
+ * Takes an element and adds it to a set. If it is already in the set,
+ * the address to that element is returned, the one to the newly
+ * inserted element otherwise.
+ */
+void *add(void *_set, const void *_element)
+{
+	int *set = _set;
+	const int *element = _element;
+
+	assert(set > heap && set < heap + MANY);
+	assert(*set == MANY); /* if it would not be MANY, something is
+				 wrong */
+	assert(element > heap && element < heap + MANY);
+
+	if (*element == MANY) /* allocated but not added */
+		*(int *)element = set - heap;
+	else                  /* already added */
+		assert(*element == set - heap);
+
+	return (void *)element;
+}
+
+
+/**
+ * @brief Looks if its element contains the proper index for the set.
+ */
+void * find(const void *_set, const void *_element)
+{
+	const int *set = _set;
+	const int *element = _element;
+
+	assert(set > heap && set < heap + MANY);
+	assert(*set == MANY);
+	assert(element > heap && element < heap + MANY);
+	assert(*element);
+
+	return *element == set - heap ? (void *) element : 0;
+}
+
+/**
+ * @brief Converts the result of find() into a truth value.
+ */
+int contains(const void *_set, const void *_element)
+{
+	return find(_set, _element) != 0;
+}
+
+/**
+ * @brief Unassigns an element from a set.
+ */
+void *drop(void *_set, const void *_element)
+{
+	int *element = find(_set, _element);
+
+	if (element)
+		*element = MANY;
+
+	return element;
+}
+
+/**
+ * @brief Compares two objects.
+ */
+int differ(const void *a, const void *b)
+{
+	return a != b;
+}
--- src/oopc/Set.h
+++ src/oopc/Set.h
@@ -1,6 +1,9 @@
 #ifndef _SET_H
 #define _SET_H
 
+/**
+ * A set is represented in its objects. Each element points to the set.
+ */
 extern const void *Set;
 
 void *add(void *set, const void *element);
